31804c151b6894b5a6e9cd050d57c2bc5789b9da
Jose M <josem.marinhernandez@gmail.com>
2023-06-15 16:37:11 -0400

    added lombok annotations to appointment class

--- src/main/java/com/app/appointment/Appointment.java
+++ src/main/java/com/app/appointment/Appointment.java
@@ -1,5 +1,28 @@
 package com.app.appointment;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
 public class Appointment {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	private String name;
+	private LocalDate date;
+	private LocalTime time;
+	private String place;
 
 }
